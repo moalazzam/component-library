cfa74c0b10043eee3c123ae9a54aac60c5370fcf
Mohammad Alazzam <mo.alazzam@gmail.com>
2020-06-21 17:45:14 +0300

    feat: convert buttons to use design tokens utils

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,28 +1,28 @@
 import styled from "styled-components";
-
-const primaryBlue = "#030086";
+import { defaultTheme } from "../utils";
+import { typeScale } from "../utils";
 
 const Button = styled.button`
   padding: 8px 12px;
-  font-size: 1rem;
+  font-size: ${typeScale.paragraph};
   border-radius: 2px;
   min-width: 100px;
   cursor: pointer;
-  font-family: "Roboto Mono", monospace;
+  font-family: ${defaultTheme.primaryFont};
 `;
 
 export const PrimaryButton = styled(Button)`
-  background-color: ${primaryBlue};
-  border: none;
-  color: white;
+  background-color: ${defaultTheme.primaryColor};
+  color: ${defaultTheme.textColorOnPrimary};
+  border: 2px solid transparent;
 `;
 
 export const SecondaryButton = styled(Button)`
-  border: 2px solid ${primaryBlue};
-  color: ${primaryBlue};
+  border: 2px solid ${defaultTheme.primaryColor};
+  color: ${defaultTheme.primaryColor};
 `;
 
 export const TertiaryButton = styled(Button)`
   border: 2px solid transparent;
-  color: ${primaryBlue};
+  color: ${defaultTheme.primaryColor};
 `;
