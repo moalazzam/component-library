1643a84baf4d180a56a0fe3b4309eded6426a90b
Mohammad Alazzam <mo.alazzam@gmail.com>
2020-06-21 16:15:33 +0300

    feat: add primary, secondary and tertiary buttons

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  PrimaryButton,
+  SecondaryButton,
+  TertiaryButton,
+} from "./components/Buttons";
+
+const App = () => (
+  <div>
+    <PrimaryButton>Hello world</PrimaryButton>
+    <SecondaryButton>Goodbye world</SecondaryButton>
+    <TertiaryButton>Hey world</TertiaryButton>
+  </div>
+);
+
+ReactDOM.render(<App />, document.querySelector("#root"));
